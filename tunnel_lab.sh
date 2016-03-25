fae7b382773d8dfc0072f6b9affc29d4390e6bf4
Éric Lemoine <elemoine@mirantis.com>
2016-03-25 07:21:04 +0100

    Better lab tunnel script

--- tunnel_lab.sh
+++ tunnel_lab.sh
@@ -2,5 +2,4 @@
 
 set -x
 
-# Fuel UI, Grafana, Kibana, Elasticsearch, Nagios
-ssh -N -L 10000:10.109.5.2:8443 -L 10001:10.109.6.4:8000 -L 10002:10.109.6.5:80 -L 9200:10.109.6.5:9200 -L 10003:10.109.6.6:8001 lab
+ssh -t -v -L 9999:localhost:9932 lab ssh -t -D 9932 root@10.109.0.2
