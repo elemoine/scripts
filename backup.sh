d2c5e6fce4ecceec2ed0cad4d10deae2044132a3
Éric Lemoine <eric.lemoine@oslandia.com>
2017-05-09 08:40:18 +0200

    backup.sh: use duplicity virtualenv

--- backup.sh
+++ backup.sh
@@ -8,8 +8,16 @@ DEV=/dev/sdb
 
 sudo mount $DEV /media/usb
 
+. $HOME/.local/bin/virtualenvwrapper.sh
+
+set +e
+workon duplicity
+set -e
+
 cd $HOME/src/backup
 ./backup.sh -d /media/usb/backup -s 5BBF59DF126FADEF -e 57F334375840CA38 -v
+
+deactivate
 
 sudo umount $DEV
 
