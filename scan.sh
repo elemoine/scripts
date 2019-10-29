2780bd8fbf7efdc63f3d5f2b635298aaf9b12b7e
Éric Lemoine <eric.lemoine@oslandia.com>
2019-10-29 21:02:21 +0100

    Improve the scan.sh script

--- scan.sh
+++ scan.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 FILE=$1
+shift
 
-hp-scan --dest=file --mode=gray --size=a4 --resolution=100 --file=${FILE}
+hp-scan --dest=file --mode=gray --size=a4 --resolution=100 --file=${FILE}.tmp $*
+ps2pdf ${FILE}.tmp ${FILE}
+rm ${FILE}.tmp
