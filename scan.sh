ecbfc138b1fc322890be2f30fa839b1b73b5d38a
Éric Lemoine <eric.lemoine@oslandia.com>
2019-11-27 07:48:01 +0100

    Improve scan.sh

--- scan.sh
+++ scan.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-FILE=$1
+FILEPATH=$1
 shift
 
-hp-scan --dest=file --mode=gray --size=a4 --resolution=100 --file=${FILE}.tmp $*
-ps2pdf ${FILE}.tmp ${FILE}
-rm ${FILE}.tmp
+FILEPATH_NO_EXTENSION=${FILEPATH%.*}
+EXTENSION=${FILEPATH##*.}
+
+TEMP_FILEPATH=${FILEPATH_NO_EXTENSION}-tmp.${EXTENSION}
+
+hp-scan --dest=file --mode=gray --size=a4 --resolution=100 --file=${TEMP_FILEPATH} $*
+ps2pdf ${TEMP_FILEPATH} ${FILEPATH}
+rm ${TEMP_FILEPATH}
