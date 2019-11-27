86eca63083a9f70c7c9683ccb314df069dfa6014
Éric Lemoine <eric.lemoine@oslandia.com>
2019-11-27 08:21:15 +0100

    Improve the usb-key.sh script

--- usb-key.sh
+++ usb-key.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 action=$1
+dev=$2
 
 usage() {
-    echo "$0 [-o|-c]"
+    echo "$0 -o|-c [dev]"
     exit 1
 }
 
-[[ -n $action ]] || usage
+[[ $action == "-o" ]] && [[ -n $dev ]] || [[ $action == "-c" ]] || usage
 
 if [[ $action == "-c" ]]
 then
-    sudo umount /media/usb0
+    sudo umount /media/usb
     sudo cryptsetup luksClose usbkey1
 else
-    sudo cryptsetup luksOpen /dev/sdc1 usbkey1
+    sudo cryptsetup luksOpen $dev usbkey1
     sudo mount /dev/mapper/usbkey1 /media/usb
 fi
 
