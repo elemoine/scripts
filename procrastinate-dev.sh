dd61ace3120dd4663598b34ba0f54737d1cebf2b
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-03-13 10:28:59 +0100

    Add tmux script for procrastinate dev

---
+++ procrastinate-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tmux new-session -s procrastinate -n git -d
+tmux send-keys -t procrastinate:git 'cd ~/src/procrastinate && vex main docker-compose up -d postgres && vex main' C-m
+
+tmux new-window -n edit
+tmux send-keys -t procrastinate:edit 'cd ~/src/procrastinate && vex main' C-m
+
+tmux new-window -n test
+tmux send-keys -t procrastinate:test 'cd ~/src/procrastinate && PGHOST=localhost PGUSER=postgres PGDATABASE=procrastinate vex procrastinate' C-m
+
+tmux new-window -n psql
+tmux send-keys -t procrastinate:psql 'cd ~/src/procrastinate && sleep 3 && PGHOST=localhost PGUSER=postgres PGDATABASE=procrastinate psql' C-m
+
+tmux select-window -t git
+tmux attach -t procrastinate
