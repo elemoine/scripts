589d4cd5669a377f4dead60ee3bf35076aa9c289
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-06-30 19:06:56 +0200

    Update procrastinate-dev.sh

--- procrastinate-dev.sh
+++ procrastinate-dev.sh
@@ -7,10 +7,10 @@ tmux new-window -n edit
 tmux send-keys -t procrastinate:edit 'cd ~/src/procrastinate && vex main' C-m
 
 tmux new-window -n test
-tmux send-keys -t procrastinate:test 'cd ~/src/procrastinate && PGHOST=localhost PGUSER=postgres PGDATABASE=procrastinate vex procrastinate' C-m
+tmux send-keys -t procrastinate:test 'cd ~/src/procrastinate && PGHOST=localhost PGUSER=postgres PGPASSWORD=password PGDATABASE=procrastinate vex procrastinate' C-m
 
 tmux new-window -n psql
-tmux send-keys -t procrastinate:psql 'cd ~/src/procrastinate && sleep 3 && PGHOST=localhost PGUSER=postgres PGDATABASE=procrastinate psql' C-m
+tmux send-keys -t procrastinate:psql 'cd ~/src/procrastinate && sleep 3 && PGHOST=localhost PGUSER=postgres PGPASSWORD=password PGDATABASE=procrastinate psql' C-m
 
 tmux select-window -t git
 tmux attach -t procrastinate
