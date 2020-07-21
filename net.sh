35f6c19a93cb45ef2df493a0147c37428515980a
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-07-21 13:30:47 +0200

    Change net.sh for handling wifi only

--- net.sh
+++ net.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-action=$1
-wpa_network=$2
+iface="wlp2s0"
+wpa_network=$1
 
-ethe="enp0s31f6"
-wifi="wlp2s0"
-
-if [[ $action == "-w" ]]; then
-    if [[ $(cat /sys/class/net/$wifi/operstate) == "down" ]]; then
-        sudo systemctl stop dhcpcd@${ethe}
-        sudo ip link set ${ethe} down
-        sudo systemctl start wpa_supplicant@${wifi}
-        sudo systemctl start dhcpcd@${wifi}
-        sleep 4
-    fi
-    if [[ -z $wpa_network ]]; then
-        sudo wpa_cli -i ${wifi} list_networks
-    else
-        sudo wpa_cli -i ${wifi} select_network $wpa_network
-    fi
-else
-    if [[ $(cat /sys/class/net/$ethe/operstate) == "down" ]]; then
-        sudo systemctl stop dhcpcd@${wifi}
-        sudo systemctl stop wpa_supplicant@${wifi}
-        sudo systemctl start dhcpcd@${ethe}
-    fi
+if [[ -n $wpa_network ]]; then
+    sudo wpa_cli -i ${iface} select_network $wpa_network
 fi
+
+sudo wpa_cli -i ${iface} list_networks
 
 exit 0
