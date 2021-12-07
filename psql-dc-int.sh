fb3051e146b9c7fb33abcb5899f77be85211b37d
Éric Lemoine <eric.lemoine@getalma.eu>
2021-12-07 17:13:45 +0000

    Remove unused scripts

--- psql-dc-int.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-psql postgresql://jenkins:tah3Aich9keina5u@localhost:5433/classifier
--- start-task.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for pod in $(kubectl get pods -l app=lma -o=name); do
-  kubectl exec -it -c snap ${pod#pod/} -- snapctl task create -t /etc/snap/auto/task.json
-done
