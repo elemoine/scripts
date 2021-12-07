4c09ccc195d2b44643102f4d275c3249f78c973c
Éric Lemoine <eric.lemoine@getalma.eu>
2021-12-07 17:12:36 +0000

    Remove unused scripts

--- openconnect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-~/.virtualenvs/main/bin/totp ulti | sudo openconnect --protocol=gp -u EricLe pa-gp.ultimatesoftware.com --csd-wrapper=/usr/libexec/openconnect/hipreport.sh
--- vpnc.sh
+++
@@ -1,1 +0,0 @@
-sudo vpnc --no-detach ulti-ipsec
