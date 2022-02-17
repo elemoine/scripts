1ea01abdf84220640990b2e816d839034d5c43db
Éric Lemoine <eric.lemoine@getalma.eu>
2022-02-17 11:11:38 +0100

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 iface="wlp0s20f3"
-wpa_network=$1
+wpa_network=${1:-0}
 
-if [[ -n $wpa_network ]]; then
-    sudo wpa_cli -i ${iface} select_network $wpa_network
-fi
-
+sudo wpa_cli -i ${iface} select_network ${wpa_network}
 sudo wpa_cli -i ${iface} list_networks
 
 exit 0
