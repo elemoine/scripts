dd7ea87ca3aa1c9150154ed2db7212964fb0cd08
Éric Lemoine <eric.lemoine@getalma.eu>
2023-09-16 10:42:33 +0200

    Improve backup_home.sh

--- backup_home.sh
+++ backup_home.sh
@@ -13,17 +13,29 @@ mount | grep "on $MNTPT" > /dev/null
 mounted=$?
 set -e
 
-if [[ $mounted -ne 0 ]]; then
+function mount_disk()
+{
+    echo "Mounting $DEV..."
     pmount $DEV
+}
+
+function umount_disk()
+{
+    echo "Umounting $DEV..."
+    pumount $DEV
+}
+
+if [[ $mounted -ne 0 ]]; then
+    mount_disk
+else
+    echo "$DEV already mounted"
 fi
+
+trap umount_disk EXIT
 
 [[ -f $HOME/.restic_password_file ]] && RESTIC_PASSWORD_FILE="$HOME/.restic_password_file"
 
 cd $HOME/src/backup
 RESTIC_PASSWORD_FILE=$RESTIC_PASSWORD_FILE ./backup.sh -d $MNTPT/backup_home_alma -v
-
-if [[ $mounted -ne 0 ]]; then
-    pumount $DEV
-fi
 
 exit 0
